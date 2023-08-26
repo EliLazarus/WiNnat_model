b9ef146c42f1335c69f37ad5540bb2b088059628
EliLazarus <lazarus.eli@gmail.com>
2023-08-26 14:44:10 -0700

    include Endow(PA)

--- WiNDC_National.jl
+++ WiNDC_National.jl
@@ -10,7 +10,7 @@ n=8
 	yr = S[:yr] # "Years in WiNDC Database",
 	sectorsi  = S[:i][1:n] # "BEA Goods and sectors categories", is "i" in GAMS
 	sectorsj = copy(sectorsi) # "BEA Goods and sectors categories", is "j" in GAMS, somehow different
-	fd = S[:fd] # "BEA Final demand categories",
+	xfd = S[:fd] # "BEA Final demand categories",
 	ts = S[:ts] # "BEA Taxes and subsidies categories",
 	valueadded = S[:va] # "BEA Value added categories excluding othtax", va in GAMS
 	margin  = S[:m] # "Margins (trade or transport)"; m in GAMS
@@ -19,14 +19,13 @@ n=8
 
 	year = Symbol(2017)
 	# PARAMETERS
-	# TODO: It's not cleat that this is filling the values appropriately
 	# ty = add!(WiNnat, Parameter(:ty, indices = (sectorsj,), value=P[:ty_0][year,:].data)) #	"Output tax rate",
 	ta = add!(WiNnat, Parameter(:ta, indices = (sectorsi,), value=P[:ta_0][year,sectorsi].data)) #	"Tax net subsidy rate on intermediate demand",
 	tm = add!(WiNnat, Parameter(:tm, indices = (sectorsi,), value=P[:tm_0][year,sectorsi].data)) #	"Import tariff";
 
 	yr = Symbol(2017)
 
-	# PARAMETERS For a single year, knock out one dimension
+	# Data For a single year, knock out one dimension
 		y_0 = P[:y_0][yr,:] #	"Gross output",
 		ys_0 = P[:ys_0][yr,:,:] #	"Sectoral supply",
 		ty_0 = P[:ty_0][yr,:] #	"Output tax rate"
@@ -45,6 +44,7 @@ n=8
 		ms_0 = P[:ms_0][yr,:,:] #	"Margin supply",
 		md_0 = P[:md_0][yr,:,:] #	"Margin demand",
 		s_0 = P[:s_0][yr,:] #	"Aggregate supply",
+		#Data Missing
 		# d_0 = P[:d_0][yr,:] #	"Sales in the domestic market",
 		a_0 = P[:a_0][yr,:] #	"Armington supply",
 		bopdef_0 = P[:bopdef_0][yr] #	"Balance of payments deficit",
@@ -103,11 +103,7 @@ n=8
 	# tm0 = add!(WiNnat, Parameter(:tm0, indices = (sectorsi,), value=P[:tm_0][year,:].data)) #	"Import tariff";
 
 	# TODO Not sure what this is?
-
 	# Looks like a filters, but I don't see where they're set.
-	# The best I can tell is that at some point of data the i and j might be different, but in the csvs I have they're exactly the same
-	# Which also seems like it must be true re the GAMS line alias(i, j);...
-
 
 	# sets	y_(j)	"Sectors with positive production",
 	# 	a_(i)	"Sectors with absorption",
@@ -121,9 +117,9 @@ n=8
 	MS = add!(WiNnat, Sector(:MS, indices=(margin,)))
 
 	# commodities:
-	# Should be filtered for sectors in $a0(i)	!
+	# Should be filtered for sectors in $a0(i)	?!
 	PA  = add!(WiNnat, Commodity(:PA, indices=(sectorsi, ))) #	Armington price
-	# Should be filtered for sectors in $py_(i)
+	# Should be filtered for sectors in $py_(i)   ?!
 	PY  = add!(WiNnat, Commodity(:PY, indices=(sectorsi,))) #	!	Supply
 	PVA = add!(WiNnat, Commodity(:PVA, indices=(valueadded,))) #		!	Value-added
 	PM  = add!(WiNnat, Commodity(:PM, indices=(margin,))) #		!	Margin
@@ -137,14 +133,11 @@ n=8
 		@production(WiNnat, Y[j], 0., 0.,
 		[Output(PY[i], ys_0[j,i], taxes=[Tax(ty_0[j], RA)]) for i in sectorsi], 
 		[
-			[Input(PA[i], id_0[i,j]) for i in sectorsi]
-		;
+		 [Input(PA[i], id_0[i,j]) for i in sectorsi];
+		#For testing without nesting
 		[Input(PVA[va], va_0[va,j]) for va in valueadded]
 		]
-		
-		
-	# 	# [		[Input(PA[i], P[:id_0][year,i,j]) for i in sectorsi];                        
-	#     # 	[Input(
+	#   # 	[Input(
 	# 	# 				Nest(:VA, 1.,
 	# 	# 				sum(P[:va_0][year,va,j] for va in valueadded),
 	# 	# 					[Input(PVA[va], P[:va_0][year,va,j]) for va in valueadded]),
@@ -157,21 +150,22 @@ n=8
 
 	for m in margin
 		add!(WiNnat, Production(MS[m], 0., 1., 
-		[Output(PM[m], sum(ms_0[i,m] for i in sectorsi) ) for m in margin],# in sectorsi],  # *&* "for i in sectors i" for the sum?
+		[Output(PM[m], sum(ms_0[i,m] for i in sectorsi) ) for m in margin],
 		[Input(PY[i], ms_0[i,m]) for i in sectorsi]))
 	end
 
 	for i in sectorsi 
-		add!(WiNnat, Production(A[i], 2., 0.,
-		# @production(WiNnat, A[i], 2., 0.,
+		# add!(WiNnat, Production(A[i], 2., 0.,
+		@production(WiNnat, A[i], 2., 0.,
 		[[Output(PA[i], a_0[i], taxes=[Tax(:($(ta[i])*1), RA)], price=(1-ta_0[i])) for i in sectorsi]; # Question re ta and ta0
 		Output(PFX, x_0[i])],
 
 		#For testing without nesting
 		[[Input(PY[i], y_0[i]) for i in sectorsi];
-		[Input(PFX, m_0[i], taxes=[Tax(:($(tm[i])*1), RA)], price=(1+tm_0[i]))];
-		[Input(PM[m], md_0[m,i]) for m in margin]]
-		))
+		 [Input(PFX, m_0[i], taxes=[Tax(:($(tm[i])*1), RA)], price=(1+tm_0[i]))];
+		 [Input(PM[m], md_0[m,i]) for m in margin]]
+		)
+		# )
 
 		# [[Input(
 		# 	Nest(:dm, 2.,
@@ -185,14 +179,10 @@ n=8
 	add!(WiNnat, DemandFunction(RA, 1.,
 		[Demand(PA[i], fd_0[i,:pce]) for i in sectorsi],
 		[[Endowment(PY[i], fs_0[i]) for i in sectorsi];
-		#Endowment(PA[i], -sum(P[:fd_0][year,i,:])+ P[:fd_0][year,i,:pce] for i in sectorsi), 
-
-		   #xfd seems like it's fd_0[i], but without the fd,fd.
-		 [Endowment(PVA[va], sum(va_0[va,:])) for va in valueadded]
-		 ;
+		 [Endowment(PA[i], -sum(fd_0[i,x] for x in xfd)) for i in sectorsi];
+		 [Endowment(PVA[va], sum(va_0[va,:])) for va in valueadded];
 		 Endowment(PFX, bopdef_0)
 		]))
-	# LAST THING (for first pass), What syntax to sum of j (meaning pick a j, sum all of va for that j, next j, sum all of va for that j etc. ?)
 
 	# solve!(WiNnat, cumulative_iteration_limit=0)
 
