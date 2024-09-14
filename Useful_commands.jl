1a0291f2a5d434eade24a145bbb1dfade2518dc6
EliLazarus <lazarus.eli@gmail.com>
2024-09-13 19:00:22 -0700

    more useful

--- Useful_commands.jl
+++ Useful_commands.jl
@@ -7,11 +7,15 @@ for (a,b) in enumerate(array/Dict)
        print("Numerical index $a, value $b")
 end
 
-WiNnat.productions[collect(keys(WiNnat.productions))[11]]  
+WiNnat.productions[collect(keys(WiNnat.productions))[11]]  #or
+MPSGE.production_sectors(MultiNat)[1]
 
 compensated_demand(Y[:ppd], PY[:air])
 value(compensated_demand(Y[:ppd], PY[:air]))
-
+demand(RA,PA[:agr])
+value(demand(RA,PA[:agr]))
+sum([value(demand(RA,PA[i])) for i in I])
+compensated_demand(WiNnat[:A][:agr], WiNnat[:PFX], :dm) # dm here specifies the nest name, useful if the same commodity appears in different nests
 ## Model name prints Dict of MPSGEModelstruct
 
 print([keys(ta_0[Symbol("2017"), :]) ta_0[Symbol("2017"),:]])
@@ -21,5 +25,8 @@ only(filter(:var => ==(:TotEm), fullvrboth)[:value])
 print(filter(row -> row.var ∈ [Symbol("Y[alt]"),Symbol("Y[cep]"),Symbol("Y[ote]"),Symbol("Y[mmf]"),Symbol("Y[rec]"),Symbol("Y[min]"),Symbol("Y[pmt]"),Symbol("Y[sle]"),Symbol("Y[pet]"),Symbol("Y[oil]"),Symbol("Y[wst]"),Symbol("Y[agr]")], Compare))
 
 print(filter(row -> row.var ∈ [Symbol("Y[alt]"),Symbol("Y[cep]"),Symbol("Y[ote]"),Symbol("Y[mmf]"),Symbol("Y[rec]"),Symbol("Y[min]"),Symbol("Y[pmt]"),Symbol("Y[sle]"),Symbol("Y[pet]"),Symbol("Y[oil]"),Symbol("Y[wst]"),Symbol("Y[agr]")], Compare))
+
+set_silent(MultiNat)
+unset_silent(MultiNat)
 
 collect(1:.2:10) #turn iterator into vector
