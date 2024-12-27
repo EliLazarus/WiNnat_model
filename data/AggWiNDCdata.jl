c8b0ee29b7b637842f1f5ab4d8245fcab0b4fb3f
EliLazarus <lazarus.eli@gmail.com>
2024-12-26 21:35:58 -0800

    clean up agg file

--- data/AggWiNDCdata.jl
+++ data/AggWiNDCdata.jl
@@ -3,8 +3,6 @@ using WiNDC, DataFrames, MPSGE, GamsStructure, CSV
 ##cd to top level of the data files
 raw_data_directory = "./detailed_data/national"
 
-# aggregation in [:summary, :detailed]. I'd like to have this be a wider range
-# of options.
 all_national_data = WiNDC.national_tables(raw_data_directory; aggregation = :detailed);
 all_summary_national_data = WiNDC.national_tables(raw_data_directory; aggregation = :summary);
 # For the summary level data, no issue using all years. Several years are failing
@@ -33,18 +31,15 @@ callibrated_national_data_summary,MS = calibrate(raw_national_summary_data)
 # Apparently there is an error here using the summary data. I have the value added
 # categories hard coded... which is super smart of me! I should fix that.
 # M = national_mpsge(national_data)
-
-
 # Benchmark
 # solve!(M, cumulative_iteration_limit = 0)
-
 
 # There are three parameters you can experiment with
 #
 # Absorption_tax[commodities]
 # Output_tax[sectors]
 # Margin_tax[commodities]
-#
+
 #These default to their values in the dataset. 
 
 # set_value!.(M[:Output_tax], .5*value.(M[:Output_tax]))
@@ -75,27 +70,23 @@ vcat(get_subtable.(Ref(callibrated_national_data), ["intermediate_demand", "valu
 # You should notice there is no subtable for value added.
 get_table(callibrated_national_data) |>
     x -> unique(x, :subtable)
-
 # This is because value added is a composite table, it consists of several smaller
 # subtables. To see which ones use get_set, the elements comprise value added.
 get_set(callibrated_national_data, "value_added")
-
 # If we look at labor_demand we will see two elements, labor demand and V00100. 
 # The V00100 is the NAICS code for labor compensation. 
 get_set(callibrated_national_data, "labor_demand")
 
 # example correspondence table
-# X = DataFrame([["111A0","1111B0"], ["A","A"]], [:commodities, :name])
+# Currently set to replicate the WiNDC summary, but can be any abitraray mapping been BEA_detail, and any aggregation set
 Codes=CSV.read(joinpath(raw_data_directory,"./BEA_WiNDC_Detail-Summary_codes.csv"), DataFrame, header=1)
-# Codes=[Symbol.(Codes[:,1:4]) Codes[:,5]] NOT GOOD -> raw_national_data has Strings
-
 X = DataFrame([Codes[:,:BEA_detail], Codes[:,:WiNDC]], [:commodities, :name])
 
-## callibrate first=> get_table(callibrated_national_data)
-test_subtables = ["intermediate_supply", "intermediate_demand","labor_demand","capital_demand","other_tax"]
+## To callibrate first=> get_table(callibrated_national_data), or get_table(raw_national_data) and callibrate below
+double_aggregate_subtables = ["intermediate_supply", "intermediate_demand","labor_demand","capital_demand","other_tax"]
 TMP = NationalTable( vcat(
     get_table(callibrated_national_data) |>
-    x -> filter(:subtable => !(in(test_subtables)), x) |>
+    x -> filter(:subtable => !(in(double_aggregate_subtables)), x) |> #ggregate the simple tables separately
     x -> leftjoin(
         x,
         X,
@@ -107,10 +98,9 @@ TMP = NationalTable( vcat(
     ) |>
     x -> groupby(x, [:commodities, :sectors, :year, :subtable]) |>
     x -> combine(x, :value => sum => :value),
-
-
+# Now aggregate the tables that also need to aggregate sectors, in 2 steps
     get_table(callibrated_national_data) |>
-    x -> filter(:subtable => (in(test_subtables)), x) |>
+    x -> filter(:subtable => (in(double_aggregate_subtables)), x) |>
     x -> leftjoin(
         x,
         X,
@@ -136,53 +126,7 @@ TMP = NationalTable( vcat(
         callibrated_national_data.sets
 )
 
-# TMP.table[findall(in(test_subtables), TMP.table.subtable), :] =
-# print(filter(:subtable => !(in(test_subtables)), TMP.table))
-# # TMP.table = vcat(filter(:subtable => !(in(test_subtables)), TMP.table),
-#  filter(:subtable => in(test_subtables), TMP.table) |>
-#         x -> leftjoin(
-#         x,
-#         X,
-#         on = :sectors=>:commodities,
-#         renamecols = "" => "_com"
-#     ) |>
-#     x -> transform!(x,
-#         [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-#     ) |>
-#     x -> groupby(x, [:commodities, :sectors, :subtable, :year]) |>
-#     x -> combine(x, :value => sum => :value))
-# # get_table(callibrated_national_data)[findall(in(test_subtables), TMP.table.subtable), :]
-# get_subtable(TMP, "intermediate_supply") |>
-# df[findall(in(["stringA", "stringB", "stringC"]), df.col), :]
-# TMP = NationalTable(
-#     TMP.table[findall(in(test_subtables), TMP.table.subtable), :] |> 
-#         x -> leftjoin(
-#         x,
-#         X,
-#         on = :sectors=>:commodities,
-#         renamecols = "" => "_com"
-#     ) |>
-#     x -> transform!(x,
-#         [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-#     ) |>
-#     x -> groupby(x, [:commodities, :sectors, :subtable, :year]) |>
-#     x -> combine(x, :value => sum => :value),
-# TMP.sets)
-    
-# testys.commodities = Symbol.(testys.commodities); testys.sectors = Symbol.(testys.sectors)   
-
-# TMP,M = calibrate(TMPraw)
-
-
-
-# [i for i in sort!(unique(TMP.table[:,:sectors])) ]
-# [i for i in unique(TMP.table[:,:commodities])]
-
-
-
-
-
-
+#Take a look to check and look at the difference
 get_subtable(TMP, "labor_demand")
 get_subtable(TMP, "intermediate_demand")
 get_subtable(TMP, "exports")
@@ -191,15 +135,12 @@ get_subtable(callibrated_national_data_summary, "intermediate_demand")
 
 subs_TMP = get_table(TMP) |>
     x -> unique(x, :subtable)
-
-# function bea_io_notations() in core_data_defines.jl from WiNDC.jl itelf has BEA to WiNDC.
-
+#print the numbers for raw and aggregated
 for i in subs_TMP[:,:subtable]
     println(i,": ",length(unique(get_subtable(TMP,i)[!,:commodities])),": ",length(unique(get_subtable(raw_national_data,i)[!,:commodities]))) # Compared to get_subtable(raw_national_data,i)
 end
-TMP
-subs_TMP
 
+## Make DataFrames to compare values
 x_0[yr,:]  #	    "Exports of goods and services",
 ex_tmp = get_subtable(TMP,"exports"); ex_tmp.commodities=Symbol.(ex_tmp.commodities)
 ex = DataFrame([x_0[yr,:].axes[1] x_0[yr,:].data], [:Win, :exp]); ex.Win = Symbol.(ex.Win)
@@ -270,107 +211,25 @@ ta_tmp = WiNDC.absorption_tax_rate(TMP); ta_tmp.commodities=Symbol.(ta_tmp.commo
 ta = DataFrame([ta_0[yr,:].axes[1] ta_0[yr,:].data], [:Win, :ta]); ta.Win = Symbol.(ta.Win)
 comp_ta = outerjoin(ta_tmp,ta, on=:commodities=>:Win)
 print(comp_ta)
-# testys = get_subtable(TMP, "intermediate_supply") |>
-#     x -> leftjoin(
-#         x,
-#         X,
-#         on = :sectors=>:commodities,
-#         renamecols = "" => "_com"
-#     ) |>
-#     x -> transform(x,
-#         [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-#     ) |>
-#     x -> groupby(x, [:commodities, :sectors, :year]) |>
-#     x -> combine(x, :value => sum => :value)
-# testys.commodities = Symbol.(testys.commodities); testys.sectors = Symbol.(testys.sectors)   
 
-# "Complicated"
 ys_0[yr,:,:]   #	"Intermediate Supply",
 ys_tmp = get_subtable(TMP,"intermediate_supply"); ys_tmp.commodities=Symbol.(ys_tmp.commodities); ys_tmp.sectors=Symbol.(ys_tmp.sectors)
-# ys_tmp_win = leftjoin(ys_tmp,X, on= :sectors=>:commodities, renamecols=""=>"_com"); ys_tmp_win.commodities=Symbol.(ys_tmp_win.commodities); ys_tmp_win.name_com=Symbol.(ys_tmp_win.name_com)
 ys = stack(DataFrame([ys_0.axes[3] ys_0[yr,:,:].data], [:Win ; [x for x in I]]),2:72); ys.variable=Symbol.(ys.variable); ys.Win=Symbol.(ys.Win)
 comp_ys = rightjoin(ys_tmp, ys, on=[:commodities=>:variable, :sectors=>:Win], renamecols=""=>"_win")
 print(comp_ys[1:900,:])
-# ys = DataFrame([[i for x in  ys_0[yr,:].axes for i in x] ys_0[yr,:].data], [:Win, :ys]); ys.Win = Symbol.(ys.Win)
-# print(outerjoin(ys_tmp,y, on=:commodities=>:Win))
-
-# testid = get_subtable(TMP, "intermediate_demand") |>
-#     x -> leftjoin(
-#         x,
-#         X,
-#         on = :sectors=>:commodities,
-#         renamecols = "" => "_com"
-#     ) |>
-#     x -> transform(x,
-#         [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-#     ) |>
-#     x -> groupby(x, [:commodities, :sectors, :year]) |>
-#     x -> combine(x, :value => sum => :value)
-# testid.commodities = Symbol.(testid.commodities); testid.sectors = Symbol.(testid.sectors)   
 
 id_0[yr,:,:]  #	"Intermediate demand",
 id_tmp = get_subtable(TMP,"intermediate_demand"); id_tmp.sectors = Symbol.(id_tmp.sectors); id_tmp.commodities = Symbol.(id_tmp.commodities)
-# leftjoin(id_tmp,Symbol.(unique(Codes[:,[3,4]])),on=:sectors=>:BEA_detail)
 id = stack(DataFrame([id_0.axes[3] id_0[yr,:,:].data], [:Win ; [x for x in I]]),2:72); id.variable=Symbol.(id.variable); id.Win=Symbol.(id.Win)
 comp_id = leftjoin(id_tmp, id, on=[:commodities=>:Win, :sectors=>:variable], renamecols=""=>"_win")
 print(comp_id)
 
-# testva = append!(get_subtable(TMP,"labor_demand"),get_subtable(TMP,"capital_demand")) |>
-#     x -> leftjoin(
-#         x,
-#         X,
-#         on = :sectors=>:commodities,
-#         renamecols = "" => "_com"
-#     ) |>
-#     x -> transform(x,
-#         [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-#     ) |>
-#     x -> groupby(x, [:commodities, :sectors, :year]) |>
-#     x -> combine(x, :value => sum => :value)
-# testva.commodities = Symbol.(testva.commodities); testva.sectors = Symbol.(testva.sectors)   
-
 va_0[yr,:,:]  #	"Value added",
 va_tmp = append!(get_subtable(TMP,"labor_demand"),get_subtable(TMP,"capital_demand")); va_tmp.commodities=Symbol.(va_tmp.commodities); va_tmp.sectors=Symbol.(va_tmp.sectors)
-# va_tmp = leftjoin(va_tmp, fd_codes, on=:commodities =>:detailed)
-# va_tmp = leftjoin(va_tmp, Codes, on=:sectors =>:BEA_detail, renamecols=""=>"_win")
-# va_tmp = va_tmp[:,[1 ; 2 ;3;4;5;10]]
 va = stack(DataFrame([va_0.axes[3] transpose(va_0[yr,[va for va in VA],:].data)], [:Win; [va for va in VA]]),2:3); va.Win = Symbol.(va.Win); va.variable = Symbol.(va.variable)
 comp_va = outerjoin(testva,va, on=[:commodities=>:variable, :sectors=>:Win], renamecols=""=>"_win")
 print(comp_va)
 
-# other_tax_rate(data::AbstractNationalTable; column = :value, output = :value) = 
-# testothtax = get_subtable(TMP, "other_tax") |>
-# x -> leftjoin(
-#     x,
-#     X,
-#     on = :sectors=>:commodities,
-#     renamecols = "" => "_com"
-# ) |>
-# x -> transform(x,
-#     [:sectors, :name_com] => ByRow((x,y) -> ismissing(y) ? x : y) => :sectors,
-# ) |>
-# x -> groupby(x, [:commodities, :sectors, :year]) |>
-# x -> combine(x, :value => sum => :value)
-# testothtax.commodities = Symbol.(testothtax.commodities); testothtax.sectors = Symbol.(testothtax.sectors)   
-
-# # testystest = copy(testys)    
-# testother_tax_rate = outerjoin(
-#     rename!(testys, :value=>:is) |>
-#             x -> groupby(x, filter(y -> y!=:commodities, [:commodities, :sectors, :year])) |>
-#             x -> combine(x, :is => sum => :is),
-#         rename!(testothtax,:value => :ot) |>
-#             x -> select(x, Not(:commodities)) |>
-#             x -> groupby(x, [:sectors, :year]) |>
-#             x -> combine(x, :ot => sum => :ot),
-#         on = filter(y -> y!=:commodities, [:commodities, :sectors, :year])
-#     ) |>
-#     x -> coalesce.(x,0) |>
-#     x -> transform(x,
-#         [:is, :ot] => ByRow((i,o) -> i == 0 ? 0 : o/i) => :value
-#     ) |>
-#     x -> select(x, Not(:is,:ot))
-# testother_tax_rate.sectors = Symbol.(testother_tax_rate.sectors)   
-## Existing Taxes
 ty_0[yr,:]  #	"Output tax rate"
 ty_tmp = WiNDC.other_tax_rate(TMP); ty_tmp.sectors=Symbol.(ty_tmp.sectors)
 ty = DataFrame([ty_0[yr,:].axes[1] ty_0[yr,:].data], [:Win, :ty]); ty.Win = Symbol.(ty.Win)
@@ -385,15 +244,9 @@ filename="CompareSummary.xlsx"
 XLSX.openxlsx(filename, mode="w") do creation_of_the_excelfile
 end
 function exp_to_xlsx(df, dfname, filename="CompareSummary.xlsx")
-    # XLSX.openxlsx(filename, mode="w") do xf
     XLSX.openxlsx(filename,mode="rw") do xf
     XLSX.addsheet!(xf,dfname)
-    XLSX.writetable!(xf[dfname], df)#, anchor_cell=XLSX.CellRef("A1"))
-    # xf[dfname]=df
-    # sheet = xf[dfname]
-    # for r in 1:size(df,1), c in 1:size(df,2)
-        #  sheet[XLSX.CellRef(r , c )] = df[r,c]
-    # end
+    XLSX.writetable!(xf[dfname], df)
 end
 end
 
